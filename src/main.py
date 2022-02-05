1f4a50b96f0bcccbc7c2316abaae1bf4d5e22e6c
jamie <rmk1075@gmail.com>
2022-02-06 01:18:27 +0900

    feat: api 적용 - papago api 연동 - resource 에 key.json 에서 클라이언트 id, 시크릿키 로드 - 문장에 대한 번역 api 호출하여 결과 반환 - 에외 처리 (RuntimeErro 추가)
    TODO:
    - separator 확인 필요
    - 온점이 한국과 다름
    - 문장 기호, 따옴표 등에 대한 고려

--- src/main.py
+++ src/main.py
@@ -1,23 +1,85 @@
+import json
 import os
 import pathlib
+import requests
 
-def main(root=str(pathlib.Path(os.path.join("/", os.path.dirname(os.path.abspath(__file__)), "../resource")).resolve())):
-    if not os.path.exists(root) or not os.path.isdir(root):
-        print("warning :: invalid root path. root={}".format(root))
+
+class Papago:
+    def __init__(self, resource=str(pathlib.Path(os.path.join("/", os.path.dirname(os.path.abspath(__file__)), "../resource")).resolve())):
+        self._path = os.path.join("/", resource, "key.json")
+        self._id, self._key = self.read_key(self._path)
+
+    def read_key(self, path):
+        with open(path, "r") as file:
+            jf = json.load(file)
+            if "id" not in jf or "key" not in jf:
+                raise RuntimeError("error :: invalid key file. please check key.json. jf={}".format(jf))
+            return jf["id"], jf["key"]
+
+    def translate(self, text):
+        method = "POST"
+        url = "https://openapi.naver.com/v1/papago/n2mt"
+
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+            "X-Naver-Client-Id": self._id,
+            "X-Naver-Client-Secret": self._key,
+        }
+
+        source = "zh-CN"
+        target = "ko"
+        data = {
+            "source": source,
+            "target": target,
+            "text": text
+        }
+
+        result = requests.request(
+            method=method,
+            url=url,
+            headers=headers,
+            data=data
+        )
+
+        if result.status_code != 200:
+            raise RuntimeError("error :: api request failed.\n url={}\n headers={}\n data={}\n result={}\n result.text={}".format(url, headers, data, result, result.text))
+
+        return result.json()["message"]["result"]["translatedText"]
+
+
+def main(resource=str(pathlib.Path(os.path.join("/", os.path.dirname(os.path.abspath(__file__)), "../resource")).resolve())):
+    if not os.path.exists(resource) or not os.path.isdir(resource):
+        print("warning :: invalid resource path. resource={}".format(resource))
         return
 
-    for file_name in os.listdir(root):
-        file_path = os.path.join("/", root, file_name)
-        print(file_path)
-        if os.path.isdir(file_path):
-            continue
+    try:
+        api = Papago(resource)
+        for file_name in os.listdir(resource):
+            file_path = os.path.join("/", resource, file_name)
+            if os.path.isdir(file_path):
+                continue
 
-        if file_path.split(".")[-1] != "txt":
-            continue
-        
-        with open(file_path, "r") as file:
-            for line in file.readlines():
-                print(line)
+            if file_path.split(".")[-1] != "txt":
+                continue
+            
+            with open(file_path, "r") as file:
+                for line in file.readlines():
+                    line = line.strip()
+                    # TODO: consider separator
+                    # for sentence in line.split("! | 。 | ?"):
+                    for sentence in line.split("。"):
+                        if len(sentence) == 0:
+                            print()
+                            continue
+
+                        translated_text = api.translate(sentence)
+
+                        print(sentence)
+                        print(translated_text)
+                        print()
+    except Exception as e:
+        print("exception={}".format(e))
+
 
 if __name__ == "__main__":
     # constant
