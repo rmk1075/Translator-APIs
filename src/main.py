929633073b45f47121af6f1375533a1a0e0016df
jamie <rmk1075@gmail.com>
2022-02-07 00:52:00 +0900

    feat: google translation 추가

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from google.cloud import translate_v2 as google_translate
 
 import json
 import os
@@ -23,8 +24,8 @@ class ApiFactory:
 
         if api_type == cls.Papago:
             return Papago(api_type=api_type, resource=resource)
-        # elif api_type == cls.GT:
-        #     return GoogleTranslation(api_type=api_type, resource=resource)
+        elif api_type == cls.GT:
+            return GoogleTranslation(api_type=api_type, resource=resource)
 
 '''
 Api 클래스
@@ -33,16 +34,14 @@ Api 클래스
 '''
 class Api:
     @classmethod
-    def read_key(cls, path, api_type):
+    def read_info(cls, path, api_type):
         with open(path, "r") as file:
             jf = json.load(file)
             if api_type not in jf:
                 raise RuntimeError("error :: key file does not contains thie api account info. please check key.json. api_type={} jf={}".format(api_type, jf))
 
-            account = jf[api_type]
-            if "id" not in account or "key" not in account:
-                raise RuntimeError("error :: invalid account info. please check key.json. jf={}".format(account))
-            return account["id"], account["key"]
+            info = jf[api_type]
+            return info
 
     @abstractmethod
     def translate(self, text):
@@ -50,14 +49,43 @@ class Api:
 
 
 '''
+GoogleTranslation 클래스
+- Google Translation API 를 사용하는 번역기 클래스
+- google cloud 패키지 설치 및 credential 환경변수 설정
+'''
+class GoogleTranslation(Api):
+    def __init__(self, api_type="GoogleTranslation", resource=str(pathlib.Path(os.path.join("/", os.path.dirname(os.path.abspath(__file__)), "../resource")).resolve())):
+        self._api_type = api_type
+        self._path = os.path.join("/", resource, "key.json")
+        self._info = Api.read_info(path=self._path, api_type=self._api_type)
+
+        if "key" not in self._info:
+            raise RuntimeError("error :: invalid account info. please check key.json. info={}".format(self._info))
+        self._credential_path = self._info["key"]
+
+        os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = str(pathlib.Path(os.path.join("/", resource, self._credential_path)).resolve())
+        self._gt = google_translate.Client()
+
+    def translate(self, text):
+        target = "zh-CN"
+        result = self._gt.translate(text, target_language=target)
+
+        return result["translatedText"]
+
+'''
 Papago 클래스
-- Naver Papago 번역 API 를 사용한 번역을 수행하는 클래스
+- Naver Papago 번역 API 를 사용하는 번역기 클래스
 '''
 class Papago(Api):
     def __init__(self, api_type="Papago", resource=str(pathlib.Path(os.path.join("/", os.path.dirname(os.path.abspath(__file__)), "../resource")).resolve())):
         self._api_type = api_type
         self._path = os.path.join("/", resource, "key.json")
-        self._id, self._key = Api.read_key(path=self._path, api_type=self._api_type)
+        self._info = Api.read_info(path=self._path, api_type=self._api_type)
+
+        if "id" not in info or "key" not in info:
+            raise RuntimeError("error :: invalid account info. please check key.json. info={}".format(info))
+        self._id = self._info["id"]
+        self._key = self._info["key"]
 
     def translate(self, text):
         method = "POST"
@@ -108,7 +136,8 @@ def main(root=str(pathlib.Path(os.path.dirname(os.path.abspath(__file__))).resol
         if not os.path.exists(output_path) or not os.path.isdir(output_path):
             os.makedirs(output_path)
 
-        api = ApiFactory.getApiInstance("Papago")
+        # api = ApiFactory.getApiInstance("Papago")
+        api = ApiFactory.getApiInstance("GoogleTranslation")
         for file_name in os.listdir(input_path):
             file_path = os.path.join("/", input_path, file_name)
             if os.path.isdir(file_path):
